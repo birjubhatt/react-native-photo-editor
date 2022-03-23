50a2ffe06b3dd8537ee3f609084ec3dd368f359c
birju <birjubhatt09@gmail.com>
2022-03-23 18:04:07 +0530

    Revert "check toast"
    This reverts commit ce437c468acf1c13f5859898fffe2dadf0be14f3.

--- android/src/main/java/com/ahmedadeltito/photoeditor/PhotoEditorActivity.java
+++ android/src/main/java/com/ahmedadeltito/photoeditor/PhotoEditorActivity.java
@@ -578,14 +578,10 @@ public class PhotoEditorActivity extends AppCompatActivity implements View.OnCli
         if (v.getId() == R.id.close_tv) {
             onBackPressed();
         } else if (v.getId() == R.id.add_image_emoji_tv) {
-           // mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-            Toast.makeText(this, "tv emoji click", Toast.LENGTH_SHORT).show();
-            onBackPressed();
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
         } else if(v.getId() == R.id.add_crop_tv) {
-          //  System.out.println("CROP IMAGE DUD");
-          //  startCropping();
-            Toast.makeText(this, "tv crop", Toast.LENGTH_SHORT).show();
-            onBackPressed();
+            System.out.println("CROP IMAGE DUD");
+            startCropping();
         } else if (v.getId() == R.id.add_text_tv) {
             openAddTextPopupWindow("", -1);
         } else if (v.getId() == R.id.add_pencil_tv) {
